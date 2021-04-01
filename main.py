52cdcfc5ecb21ff9340eb5519abd6c05db818249
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2021-04-01 10:54:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 '''
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+Bridget Naylor
+CECS 229 Lab 3 Part 1.7.1
 
 '''
 print ('Hello World')
 
 def my_filter( L, num ):
-    new_L = [L.append(item) for item in L if item % num != 0]
-    print(new_L)
+    new_L = []
+    [new_L.append(item) for item in L if item % num != 0]
     return new_L;
 
 L = [1,2,3,4,5,6,7,8,9,10]
 num = 2
-my_filter( L, num )
+print(my_filter( L, num ))
